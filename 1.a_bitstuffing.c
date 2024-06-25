56bf24ff83857980ecfeb7f8759540a310ad5874
Chatbot07 <158073794+Chatbot07@users.noreply.github.com>
2024-06-25 19:47:18 +0530

    1.a_bitstuffing.c

--- 1.a_bitstuffing.c
+++ 1.a_bitstuffing.c
@@ -1,41 +1,26 @@
-
 #include<stdio.h>
-int main(){
-    int n,pos=0,c=0,j,k;
-    printf("enter size\n");scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
 
-for(int i=0;i<n;i++){
-        if(a[i]==1){
-            c++;
-            if(c==5){
-                c=0;
-                pos=i+1;
-                for(int j=n;j>=pos;j--){
-                    k=j+1;
-                    a[k]=a[j];
-                    
-                }
-                n=n+1;
-                a[pos]=0;
-            }
-        }
-        else c=0;
-    }
-
-
-    printf("data after stuffing     "); 
-
-
-for(int i=0;i<n;i++) 
-{ 
-printf("%d ",a[i]) ; 
-} 
-
-//inp 12   0 1 0 1 1 1 1 1 1 0 0 1
-//op  0101111101001
-
-}
+void main(){
+int a[15];
+int i,j,k,n,c=0,pos=0;
+printf("\n enter the no of bits"); scanf("%d",&n);
+for(i=0;i<n;i++) scanf("%d",&a[i]);
+for(i=0;i<n;i++){
+if(a[i]==1){
+c++;
+if(c==5){
+pos=i+1;
+c=0;
+for(j=n;j>=pos;j--) {
+k=j+1;
+a[k]=a[j]; } 
+a[pos]=0;
+n=n+1; } }
+else
+c=0; }
+printf("\n data after stuffing");
+printf("011111110");
+for(i=0;i<n;i++)
+printf("%d",a[i]) ;
+printf("011111110");
+ }
